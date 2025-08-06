0a3c3d8d9ac388931c45ab626d743dd0021de2e8
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-06 16:09:48 -0400

    Add files via upload

---
+++ MyTest.java
@@ -0,0 +1,21 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    // @itemTitle:"Title of the item" @tests:KXITM0YCK0YE7DX8WYTTQZ4MSK293X2
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+
+    /**
+     * @itemTitle:"Title of the item"
+     * @tests: KXITM0YCK0YE7DX8WYTTQZ4MSK293X2
+     */
+    @Test
+    public void testSubtraction() {
+        assertEquals(0, 2 - 2);
+    }
+}
