3759706aa1c87c1ed3da2cbcba9bfad15466f06c
william-ketryx <williamb@ketryx.com>
2025-08-07 12:17:12 -0400

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -3,12 +3,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 /**
-* Tests that things work.    
-*
-* @itemId:TestJava
-* @itemTitle:"Test Java Item"
-* @itemHasParent:VIC-3
-*/
+ * Utility class to read sensor.
+ *
+ * @itemId:SensorReading
+ * @itemTitle:"Sensor Reading (Java)"
+ * @itemHasParent:spec-sensor-module
+ * @itemFulfills:CS-1,KD-20
+ */
 public class MyTest {
     public void testSubtraction() {
         assertEquals(0, 2 - 2);
