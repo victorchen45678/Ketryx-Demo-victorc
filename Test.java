cb2caea3ef23035b1b37163026fcc41252ee1511
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-07 15:13:20 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,8 +7,6 @@ package com.ketryx.sample;
  * @itemId:SensorReading
  * @itemTitle:"Sensor Reading (Jav2)"
  * @itemType: Test Case
- * @itemHasParent:spec-sensor-module
- * @itemFulfills:CS-1,KD-20
  */
 public class Test {
     public static int readSensor(int a, int b) {
