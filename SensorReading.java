b591d4b1d39da58a4c3a422fef4331d51e8f194e
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-07 15:53:55 -0400

    Add files via upload

---
+++ SensorReading.java
@@ -0,0 +1,15 @@
+package com.ketryx.sample;
+
+/**
+ * Utility class to read sensor.
+ *
+ * @itemId:SensorReading
+ * @itemTitle:"Sensor Reading (Java)"
+ * @itemHasParent:spec-sensor-module
+ * @itemFulfills:CS-1,KD-20
+ */
+public class SensorReading {
+    public static int readSensor(int a, int b) {
+        return a + b;
+    }
+}
