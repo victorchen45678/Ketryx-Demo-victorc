a5685d474735d65a0ef1c6daf2145df2748cac20
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-07 11:55:11 -0400

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -3,18 +3,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class MyTest {
-
-    // @itemTitle:"Title of the item" @tests:KXITM0YCK0YE7DX8WYTTQZ4MSK293X2
-    // @id:adjflkdsafjsdlkfjdsalkjf
-    @Test
-    public void testAddition() {
-        assertEquals(4, 2 + 2);
-    }
-
     /**
-     * @itemTitle:"Title of the item2"
+     * Tests that things work.
      * @tests: KXITM0YCK0YE7DX8WYTTQZ4MSK293X2
-     * @id:fjdlakjdslkvndkasfjsldk
+     * @itemTitle: "Title of the item2"
      */
     @Test
     public void testSubtraction() {
