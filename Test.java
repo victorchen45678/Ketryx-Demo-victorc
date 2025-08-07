671e3b2f3ec16ba63d291ca8223809beef0e4968
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-07 15:16:38 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,8 +6,9 @@ package com.ketryx.sample;
  *
  * @itemId:SensorReading
  * @itemTitle:"Sensor Reading (Jav2)"
- * @itemType: Test Case
  */
+
+@Test
 public class Test {
     public static int readSensor(int a, int b) {
         return a + b;
