84f9c2ca98c7b51df45e2a39f93d7f4360e7f645
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-07 12:05:48 -0400

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -2,14 +2,14 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+/**
+* Tests that things work.    
+*
+* @itemId:TestJava
+* @itemTitle:"Test Java Item"
+* @itemHasParent:VIC-3
+*/
 public class MyTest {
-    /**
-     * Tests that things work.    
-     *
-     * @itemId:TestJava
-     * @itemTitle:"Test Java Item"
-     * @itemHasParent:VIC-3
-     */
     public void testSubtraction() {
         assertEquals(0, 2 - 2);
     }
