01f507a6d7ef7ceb8aa718f36c8b14b2b5cd1985
william-ketryx <williamb@ketryx.com>
2025-08-07 13:01:27 -0400

    Create createSensorWarning.ts

---
+++ createSensorWarning.ts
@@ -0,0 +1,12 @@
+/**
+ * When either of the following conditions is met, the system shall issue a warning notification to the user through an appropriate user interface:
+ * a) The sensor reading exceeds the predefined upper or lower limits.
+ * b) The latest entered sensor reading is outdated according to the defined maximum allowable time interval.
+
+ * @itemId:spec-sensor-reading-warning
+ * @itemTitle:"Sensor Reading Warning (Javascript)"
+ * @itemFulfills:CS-1,KD-13
+ * @itemHasParent:spec-sensor-module
+ */
+export function createReadingWarning() {
+}
