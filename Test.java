c9250d785d478e965153715eebe1e76ef6ab26a2
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-07 15:44:38 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,15 @@
 
 package com.ketryx.sample;
 
-/**
- * Utility class to read sensor.
- * @tests:rq-example-001
- * @itemId:SensorReading
- */
 
-@Test
+
 public class Test {
+    /**
+     * Utility class to read sensor.
+     * @tests:rq-example-001
+     * @itemId:SensorReading
+     */
+    @Test
     public static int readSensor(int a, int b) {
         return a + b;
     }
