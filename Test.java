c912c42ca99ac609f599f297827212a5117c949f
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2025-08-07 15:21:00 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,6 +5,7 @@ package com.ketryx.sample;
  * Utility class to read sensor.
  * @tests:KXITM4AFVQ7GZR18Z1TRJ6H92TA2V5D
  * @itemId:SensorReading
+ * @itemType: Test Case
  * @itemTitle:"Sensor Reading (Jav4)"
  */
 
